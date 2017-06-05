201eb8913a5836d15a4bc28a0276cdd3b9de0780
Victor Allen <vamuigua@gmail.com>
2017-06-05 12:59:58 +0300

    Added image button function

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    }
+  }
 });
