7f64ff1025db34723bb1399c5396c255d744e97c
Victor Allen <vamuigua@gmail.com>
2017-06-08 15:04:56 +0300

    Modified destroyRental in rental.js

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -15,7 +15,12 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
     destroyRental(rental) {
-      rental.destroyRecord();
+      var review_deletions = rental.get('reviews').map(function(review) {
+        return review.destroyRecord();
+      });
+      Ember.RSVP.all(review_deletions).then(function() {
+        return rental.destroyRecord();
+      });
       this.transitionTo('index');
     },
     destroyReview(review) {
