07703d0a38f75230eb0316ab187bd08d90da2f3c
Victor Allen <vamuigua@gmail.com>
2017-06-08 12:24:39 +0300

    Added a rental attribute on the review model to refer to one of our rental objects

--- app/models/rental.js
+++ app/models/rental.js
@@ -6,5 +6,5 @@ export default DS.Model.extend({
   type: DS.attr(),
   image: DS.attr(),
   bedrooms: DS.attr(),
-  reviews: DS.hasMany('review', { async: true })
+  rental: DS.belongsTo('rental', { async: true }),
 });
